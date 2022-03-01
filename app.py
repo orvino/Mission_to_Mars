d4a94a3c7fe8c64d47c327c093f093da27c96631
orvino <91889241+orvino@users.noreply.github.com>
2022-02-28 18:58:14 -0600

    add app.py changes

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ def index():
 def scrape():
     mars = mongo.db.mars
     mars_data = scraping.scrape_all()
-    mars.update({},mars_data, upsert=True)
+    mars.update_one({},{"$set":mars_data}, upsert=True)
     return redirect('/', code=302)
     
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
